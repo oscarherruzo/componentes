b2dbf464a09dde07ec3939ab9f3b2cdb40b0dd5b
oscarherruzo <thelinkmeshx@gmail.com>
2022-10-14 11:14:31 +0200

    commit 14 de Octubre

---
+++ lib/models/models.dart
@@ -0,0 +1,3 @@
+export 'package:componentes/models/menu_option.dart';
+
+
---
+++ lib/screens/screens.dart
@@ -0,0 +1,5 @@
+export 'package:componentes/screens/alert_screen.dart';
+export 'package:componentes/screens/card_screen.dart';
+export 'package:componentes/screens/listview1_screen.dart';
+export 'package:componentes/screens/listview2_screen dart';
+export 'package:componentes/screens/home_screen.dart';
